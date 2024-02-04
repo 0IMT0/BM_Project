0ba5579a01ce1856b42c5e39b5824f46caed3d63
0IMT0 <imikkot@gmail.com>
2024-02-04 23:34:42 +0000

    fresh start

--- main.py
+++
@@ -1,6 +0,0 @@
-from database_access import database_login
-from date_time_GUI import create_date_selector
-import pandas as pd
-from database_collector import boalf_data_collector
-
-boalf_data_collector()
